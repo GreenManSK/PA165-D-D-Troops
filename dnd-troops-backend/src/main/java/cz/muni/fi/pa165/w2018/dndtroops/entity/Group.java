7ffc404e4be3edf590208ea834b45174dfee0e82
Lukáš Kurčík <lukas.kurcik@gmail.com>
2018-10-10 21:27:14 +0200

    Base for 1st milestone

---
+++ dnd-troops-backend/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/entity/Group.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.w2018.dndtroops.entity;
+
+
+public class Group {
+
+    public void addHero(Hero hero) {}
+}
---
+++ dnd-troops-backend/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/entity/Hero.java
@@ -0,0 +1,111 @@
+package cz.muni.fi.pa165.w2018.dndtroops.entity;
+
+
+import cz.muni.fi.pa165.w2018.dndtroops.enums.Race;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Hero entity
+ *
+ * @author Lukáš Kurčík <lukas.kurcik@gmail.com>
+ */
+@Entity
+public class Hero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Min(0)
+    private Long experience;
+
+    @NotNull
+    @Enumerated
+    private Race race;
+
+    @ManyToOne
+    private Group group;
+
+    @ManyToMany
+    private Set<Role> roles = new HashSet<>();
+
+    public Hero() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Long experience) {
+        this.experience = experience;
+    }
+
+    public Race getRace() {
+        return race;
+    }
+
+    public void setRace(Race race) {
+        this.race = race;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+        group.addHero(this);
+    }
+
+    public Set<Role> getRoles() {
+        return Collections.unmodifiableSet(roles);
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public void removeRole(Role role) {
+        roles.remove(role);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Hero)) return false;
+        Hero hero = (Hero) o;
+        return Objects.equals(name, hero.name) &&
+                race == hero.race;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, race);
+    }
+}
