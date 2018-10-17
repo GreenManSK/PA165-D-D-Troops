2f0a973dd07bb085cb476e683d636f008c4b6c86
DanoFecko <dano9500@gmail.com>
2018-10-17 11:17:11 +0200

    role entity implementation
    *Role class
    *RoleDao interface
    *RoleDaoImpl class

---
+++ dnd-troops-backend/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/dao/RoleDaoImpl.java
@@ -0,0 +1,69 @@
+package cz.muni.fi.pa165.w2018.dndtroops.dao;
+
+import cz.muni.fi.pa165.w2018.dndtroops.entity.Role;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class RoleDaoImpl implements RoleDao {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	/**
+	 * Find role by its id
+	 *
+	 * @param id of role
+	 * @return role or null if not found
+	 */
+	@Override
+	public Role getById(long id) {
+		return entityManager.find(Role.class, id);
+	}
+
+	/**
+	 * Find all roles
+	 *
+	 * @return List of all roles
+	 */
+	@Override
+	public List<Role> getAll() {
+		return entityManager.createQuery("select r from Role r", Role.class).getResultList();
+	}
+
+	/**
+	 * Stores new role
+	 *
+	 * @param role to be created
+	 */
+	@Override
+	public void create(Role role) {
+		checkNotNull(role);
+		entityManager.persist(role);
+	}
+
+	/**
+	 * Updates existing role according to the id
+	 *
+	 * @param role to be updated
+	 */
+	@Override
+	public void update(Role role) {
+		checkNotNull(role);
+		entityManager.merge(role);
+	}
+
+	/**
+	 * Removes the hero
+	 *
+	 * @param role to be deleted\
+	 */
+	@Override
+	public void delete(Role role) {
+		checkNotNull(role);
+		entityManager.remove(role);
+	}
+}
