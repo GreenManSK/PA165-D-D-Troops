ff869641511f14a65eeb0dae8fc4b0fc99d86e30
Marek Valko <valko.marek@gmail.com>
2018-11-06 10:34:11 +0100

    Created RoleDTO

--- dnd-troops-api/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/api/dto/RoleDTO.java
+++ dnd-troops-api/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/api/dto/RoleDTO.java
@@ -1,6 +1,60 @@
 package cz.muni.fi.pa165.w2018.dndtroops.api.dto;
 
+import java.util.Objects;
 
+/**
+ * Role Data Transfer Object
+ *
+ * @author Marek Valko
+ */
 public class RoleDTO {
+    private Long id;
+    private String name;
+    private String description;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RoleDTO)) return false;
+        RoleDTO roleDTO = (RoleDTO) o;
+        return Objects.equals(getName(), roleDTO.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
 }
