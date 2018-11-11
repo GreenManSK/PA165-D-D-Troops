b65f8228311501a845e52c7e4a305aa5f5a9c01a
Marek Valko <valko.marek@gmail.com>
2018-11-11 19:37:50 +0100

    Implemented Role service interface

--- dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/RoleServiceImpl.java
+++ dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/RoleServiceImpl.java
@@ -1,13 +1,43 @@
 package cz.muni.fi.pa165.w2018.dndtroops.service;
 
 
+import cz.muni.fi.pa165.w2018.dndtroops.backend.dao.RoleDao;
 import cz.muni.fi.pa165.w2018.dndtroops.backend.entity.Role;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Implementation of Role Service
+ *
+ * @author Marek Valko
+ */
 @Service
 public class RoleServiceImpl implements RoleService {
+
+    @Inject
+    private RoleDao roleDao;
+
     @Override
     public Role getById(long id) {
-        return null;
+        return roleDao.getById(id);
+    }
+
+    @Override
+    public List<Role> getAll() { return roleDao.getAll(); }
+
+    @Override
+    public void create(Role role) { roleDao.create(role);}
+
+    @Override
+    public void update(Role role) { roleDao.create(role);}
+
+    @Override
+    public void delete(long id) {
+        Role role = roleDao.getById(id);
+        if (role != null) {
+            roleDao.delete(role);
+        }
     }
 }
