a25db139685a9aba10eb881ed0496ede264c725a
RusteeKuntz <dusan.hetlerovic@gmail.com>
2018-11-13 22:34:48 +0100

    Added GroupFacadeImpl and GroupService + Impl

--- dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/GroupService.java
+++ dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/GroupService.java
@@ -2,7 +2,91 @@ package cz.muni.fi.pa165.w2018.dndtroops.service;
 
 
 import cz.muni.fi.pa165.w2018.dndtroops.backend.entity.Group;
+import cz.muni.fi.pa165.w2018.dndtroops.backend.entity.Hero;
 
+import java.util.List;
+
+/**
+ * Group service interface
+ *
+ * @author Dusan Hetlerovic
+ */
 public interface GroupService {
+
+    /**
+     * Get group by id
+     * @param id of the group
+     * @return group with given id (if exists) or null
+     */
     Group getById(long id);
+
+    /**
+     * Get group with given name
+     *
+     * @param name of the group
+     * @return group with given name
+     */
+    Group getByName(String name);
+
+    /**
+     * Get groups where no hero has any of the given roles
+     *
+     * @param roles ids of roles to be omitted
+     * @return List of groups without given roles
+     */
+    List<Group> getGroupsWithoutRoles(List<Long> roles);
+
+    /**
+     * Get all groups
+     *
+     * @return List of all existing groups
+     */
+    List<Group> getAll();
+
+    /**
+     * Stores a new group
+     *
+     * @param group to be stored
+     */
+    void create(Group group);
+
+    /**
+     * Update an existing group
+     *
+     * @param group to be updated
+     */
+    void update(Group group);
+
+    /**
+     * Deletes stored group with given id
+     *
+     * @param id of the group to be deleted
+     */
+    void delete(long id);
+
+    /**
+     * Adds an existing hero to an existing group
+     *
+     * @param groupId id of the group
+     * @param heroId id of the hero
+     */
+    void addHero(long groupId, long heroId);
+
+    /**
+     * Remove hero from existing group
+     *
+     * @param groupId id of the group
+     * @param heroId id of the hero
+     */
+    void removeHero(long groupId, long heroId);
+
+
+    /**
+     * Retrieves all heroes in a group
+     *
+     * @param id of the group
+     * @return List of heroes in the group
+     */
+    List<Hero> getHeroesFromGroup(long id);
+
 }
--- dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/GroupServiceImpl.java
+++ dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/GroupServiceImpl.java
@@ -1,12 +1,108 @@
 package cz.muni.fi.pa165.w2018.dndtroops.service;
 
+import cz.muni.fi.pa165.w2018.dndtroops.backend.dao.GroupDao;
+import cz.muni.fi.pa165.w2018.dndtroops.backend.dao.HeroDao;
 import cz.muni.fi.pa165.w2018.dndtroops.backend.entity.Group;
+import cz.muni.fi.pa165.w2018.dndtroops.backend.entity.Hero;
+import cz.muni.fi.pa165.w2018.dndtroops.backend.entity.Role;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Implementation of the Group Service Interface
+ *
+ * @author Dusan Hetlerovic
+ */
 @Service
 public class GroupServiceImpl implements GroupService {
+
+    @Inject
+    private GroupDao groupDao;
+
+    @Inject
+    private HeroDao heroDao;
+
     @Override
     public Group getById(long id) {
-        return null;
+        return groupDao.getById(id);
     }
+
+    @Override
+    public Group getByName(String name) {
+        List<Group> groups = groupDao.getAll()
+                .stream()
+                .filter(group -> group.getName().equals(name))
+                .collect(Collectors.toList());
+        return (groups.isEmpty() ? null : groups.get(0));
+    }
+
+    @Override
+    public List<Group> getGroupsWithoutRoles(List<Long> roles) {
+        return groupDao.getAll()
+                .stream()
+                .filter(group -> {
+            for (Hero h : group.getHeroes()) {
+                for (Role r : h.getRoles()) {
+                    if (roles != null && roles.contains(r.getId())) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Group> getAll() {
+        return groupDao.getAll();
+    }
+
+    @Override
+    public void create(Group group) {
+        groupDao.create(group);
+    }
+
+    @Override
+    public void update(Group group) {
+        groupDao.create(group);
+    }
+
+    @Override
+    public void delete(long id) {
+        Group group = groupDao.getById(id);
+        if (group != null) {
+            groupDao.remove(group);
+        }
+    }
+
+    @Override
+    public void addHero(long groupId, long heroId) {
+        Hero hero = heroDao.getById(heroId);
+        Group group = groupDao.getById(groupId);
+        if (hero != null && group != null) {
+            group.addHero(hero);
+            groupDao.update(group);
+        }
+    }
+
+    @Override
+    public void removeHero(long groupId, long heroId) {
+        Hero hero = heroDao.getById(heroId);
+        Group group = groupDao.getById(groupId);
+        if (group != null) {
+            group.removeHero(hero);
+            groupDao.update(group);
+        }
+    }
+
+    @Override
+    public List<Hero> getHeroesFromGroup(long id) {
+        return new ArrayList<>(groupDao.getById(id).getHeroes());
+    }
+
 }
