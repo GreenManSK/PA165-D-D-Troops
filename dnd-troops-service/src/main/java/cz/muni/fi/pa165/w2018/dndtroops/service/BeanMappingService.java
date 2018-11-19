40552fa8d9a64d9b983ec8da3b039fa3598b4449
Marek Valko <valko.marek@gmail.com>
2018-11-19 16:52:49 +0100

    Fixed mapping for Race enums

--- dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/BeanMappingService.java
+++ dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/BeanMappingService.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pa165.w2018.dndtroops.service;
 
+import cz.muni.fi.pa165.w2018.dndtroops.api.enums.Race;
 import org.dozer.Mapper;
 
 import java.util.Collection;
@@ -15,6 +16,17 @@ public interface BeanMappingService {
     <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
 
     <T> T mapTo(Object u, Class<T> mapToClass);
+
+    /**
+     * Maps between race enums
+     */
+    Race mapTo(cz.muni.fi.pa165.w2018.dndtroops.backend.enums.Race race, Class<Race> mapToClass);
+
+    /**
+     * Maps between race enums
+     */
+    cz.muni.fi.pa165.w2018.dndtroops.backend.enums.Race mapTo(Race race, Class<cz.muni.fi.pa165.w2018.dndtroops.backend.enums.Race> mapToClass);
+
 
     Mapper getMapper();
 }
