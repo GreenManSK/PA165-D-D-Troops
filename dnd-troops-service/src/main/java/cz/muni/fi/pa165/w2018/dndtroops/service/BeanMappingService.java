426892a8c48d6c045411bf665132b519df8ad651
Lukáš Kurčík <lukas.kurcik@gmail.com>
2018-11-19 15:51:49 +0100

    Enum mapping

--- dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/BeanMappingService.java
+++ dnd-troops-service/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/service/BeanMappingService.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pa165.w2018.dndtroops.service;
 
+import cz.muni.fi.pa165.w2018.dndtroops.api.enums.Race;
 import org.dozer.Mapper;
 
 import java.util.Collection;
@@ -12,9 +13,33 @@ import java.util.List;
  */
 public interface BeanMappingService {
 
-    <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
+    /**
+     * Maps the collection of source type to a collection of the target type.
+     * Maps {@code null} to {@code null}.
+     *
+     * @param source     Collection of source type data.
+     * @param mapToClass target type
+     */
+    <T> List<T> mapTo(Collection<?> source, Class<T> mapToClass);
 
-    <T> T mapTo(Object u, Class<T> mapToClass);
+    /**
+     * Maps the source object to the target type.
+     * Maps {@code null} to {@code null}.
+     */
+    <T> T mapTo(Object source, Class<T> mapToClass);
 
+    /**
+     * Maps between race enums
+     */
+    Race mapTo(cz.muni.fi.pa165.w2018.dndtroops.backend.enums.Race race, Class<Race> mapToClass);
+
+    /**
+     * Maps between race enums
+     */
+    cz.muni.fi.pa165.w2018.dndtroops.backend.enums.Race mapTo(Race race, Class<cz.muni.fi.pa165.w2018.dndtroops.backend.enums.Race> mapToClass);
+
+    /**
+     * Returns underlying mapper.
+     */
     Mapper getMapper();
 }
