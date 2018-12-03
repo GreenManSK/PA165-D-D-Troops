1085dad615bacdc61d1a44e38891667a80ad4650
RusteeKuntz <dusan.hetlerovic@gmail.com>
2018-12-03 17:28:04 +0100

    REST Troop + sample data

---
+++ dnd-troops-sample-data/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/sampledata/SampleDataLoadingFacade.java
@@ -0,0 +1,15 @@
+package cz.muni.fi.pa165.w2018.dndtroops.sampledata;
+
+/**
+ * Sample data loading facade
+ *
+ * @author Dusan Hetlerovic
+ */
+public interface SampleDataLoadingFacade {
+
+    /**
+     * Loads predefined sample data and stores them in database
+     */
+    void loadData();
+
+}
