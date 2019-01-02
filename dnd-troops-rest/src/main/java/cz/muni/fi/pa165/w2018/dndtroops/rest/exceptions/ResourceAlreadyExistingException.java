7bc7bb632d21aad27957f1bb56bbbff679c69914
Lukáš Kurčík <lukas.kurcik@gmail.com>
2019-01-02 14:39:48 +0100

    REST error format fix

--- dnd-troops-rest/src/main/java/cz/muni/fi/pa165/w2018/dndtroops/rest/exceptions/ResourceAlreadyExistingException.java
+++
@@ -1,7 +0,0 @@
-package cz.muni.fi.pa165.w2018.dndtroops.rest.exceptions;
-/**
- * @author Lukáš Kurčík <lukas.kurcik@gmail.com>
- */
-public class ResourceAlreadyExistingException extends RuntimeException {
-    
-} 
